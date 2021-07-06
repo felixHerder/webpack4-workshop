ee4e6b6a3e9a297bdb0300d90797923eb375412d
felixHerder <felixbaciu@gmail.com>
2021-07-06 17:22:01 +0300

    analyze preset

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,38 @@
-module.exports = {
-  mode: "none"
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const HTmlWebpackPlugin = require('html-webpack-plugin');
+
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const presetConfig = require('./build-utils/loadPresets')
+
+module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
+  return merge(
+    {
+      mode,
+      module: {
+        rules: [
+          {
+            test: /\.jpe?g$/,
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  limit: 5000
+                }
+              }
+            ]
+          }
+        ]
+      },
+      output: {
+        filename: "bundle.js"
+      },
+      plugins: [
+        new HTmlWebpackPlugin(),
+        new webpack.ProgressPlugin()
+      ]
+    },
+    modeConfig(mode),
+    presetConfig({ mode, presets })
+  );
 };
